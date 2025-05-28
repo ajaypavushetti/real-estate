ac5f2a497661e7d3e6149428032c8fd3b447a4be
Pavushetti Ajaychandra <ajaypavushetti123@gmail.com>
2025-05-28 21:54:28 +0530

    create a middeware and function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs' //hashing
 
-export const signup =async (req,res)=>{
+export const signup =async (req,res,next)=>{
     const {username , email , password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); //salting num-no. of rounds
     const newUser = new User({username, email ,password : hashedPassword});
@@ -10,7 +10,7 @@ export const signup =async (req,res)=>{
         res.status(201).json('user created');
     }
     catch(error){
-        res.status(500).json(error.errmsg);
+        next(error);
     }
    
  }
--- api/index.js
+++ api/index.js
@@ -20,3 +20,14 @@ app.use(express.json());
 
  app.use('/api/user',userRouter)
  app.use('/api/auth',authRouter);
+
+ //middleware
+ app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message // we can keep in es6 if both are same
+    })
+ })
